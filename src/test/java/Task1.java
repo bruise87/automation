b3547fc7ba752685a40feff847a285b86389044c
bruise87 <kokhovetc3oow@mail.ru>
2021-10-31 15:52:00 +0300

    Домашнее задание №1

---
+++ src/test/java/Task1.java
@@ -0,0 +1,37 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+public class Task1 {
+    WebDriver driver;
+    @Test
+    public void RandomPassNotEqual() {
+        System.setProperty("webdriver.chrome.driver", "DriverChrom\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://passwordsgenerator.net/ru/");
+        WebElement generateBtn = driver.findElement(By.xpath("//div[@class='button GenerateBtn']"));
+        generateBtn.click();
+        String firstPassword = driver.findElement(By.id("final_pass")).getAttribute("value");
+        Assert.assertNotEquals("", firstPassword);
+        generateBtn.click();
+        String secondPassword = driver.findElement(By.id("final_pass")).getAttribute("value");
+        Assert.assertNotEquals(firstPassword, secondPassword);
+        driver.findElement(By.id("Symbols")).click();
+        WebElement selectElem = driver.findElement(By.id("pgLength"));
+        Select select = new Select(selectElem);
+        select.selectByValue("22");
+        generateBtn.click();
+        String thirdPassword = driver.findElement(By.id("final_pass")).getAttribute("value");
+        Assert.assertEquals(22, thirdPassword.length());
+    }
+
+    @After
+    public void affterAll() {
+        driver.quit();
+    }
+}
+
